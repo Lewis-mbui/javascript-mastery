6eb7a30ee17bbc507c929a5d695da15bbd0d67ed
Lewis-mbui <lewmugo14479@gmail.com>
2025-10-09 12:23:02 +0300

    do functions day 3 exercise 1

---
+++ 02-functions/day-003/01-square.js
@@ -0,0 +1,13 @@
+/*
+Write a function expression that takes a number and returns its square.
+
+Input: square(5) → Output: 25
+
+Input: square(0) → Output: 0
+*/
+
+const square = function(num) {
+  return num * num;
+};
+
+console.log(square(5));
