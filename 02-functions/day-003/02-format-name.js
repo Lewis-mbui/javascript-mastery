8b64739921342db60e645ba2eb25fca50b49f7f9
Lewis-mbui <lewmugo14479@gmail.com>
2025-10-09 12:27:55 +0300

    do functions day 3 exercise 2

---
+++ 02-functions/day-003/02-format-name.js
@@ -0,0 +1,8 @@
+/* 
+Write an arrow function that takes a first name and 
+last name and returns a formatted full name like "John Doe".
+*/
+
+const formatName = (firstName, lastName) => firstName + ' ' + lastName;
+
+console.log(formatName('John', 'Doe'));
