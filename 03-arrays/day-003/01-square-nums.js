9223d52b0f4d2a8e00607b9324d73fa2f47813cd
Lewis-mbui <lewmugo14479@gmail.com>
2025-10-10 11:53:43 +0300

    do arrays day 3 exercise 1

---
+++ 03-arrays/day-003/01-square-nums.js
@@ -0,0 +1,12 @@
+/*
+Use map() to create a new array where each number is squared.
+
+Print both the original and new arrays.
+*/
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squared = numbers.map((num) => num * num);
+
+console.log(numbers);
+console.log(squared);
