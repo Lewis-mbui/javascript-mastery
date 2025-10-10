fee99f5a7c09930ef00ede71378cc86489c92670
Lewis-mbui <lewmugo14479@gmail.com>
2025-10-10 11:56:43 +0300

    do arrays day 3 exercise 2

---
+++ 03-arrays/day-003/02-filter-ages.js
@@ -0,0 +1,12 @@
+/*
+Use filter() to get a new array of people who are 18 or older.
+
+Print the result.
+*/
+
+const ages = [12, 18, 25, 30, 40, 15, 22];
+
+const filtered = ages.filter((age) => age >= 18);
+
+console.log(ages);
+console.log(filtered);
