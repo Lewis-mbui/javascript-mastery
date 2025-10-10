43f70390558ac93b30da5634c53ddcd73db15383
Lewis-mbui <lewmugo14479@gmail.com>
2025-10-10 13:16:12 +0300

    do objects day 3 exercise 1

---
+++ 04-objects/day-003/01-user-profile.js
@@ -0,0 +1,21 @@
+/*
+Create an object userProfile with nested details
+
+Log the user’s email.
+
+Update the city.
+
+Add a new nested property address.country.
+*/
+
+const userProfile = {
+  name: "John",
+  contact: { email: "john@example.com", phone: "123456789" },
+  address: { city: "Mombasa", zip: "00100"}
+};
+
+console.log(userProfile.contact.email);
+userProfile.address.city = 'Nyeri';
+console.log(userProfile);
+userProfile.address.country = 'Kenya';
+// console.log(userProfile);
